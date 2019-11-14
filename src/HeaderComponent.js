fb0bb077df52a28d19c252cc966afa0e5cc7ef1a
RIDWAN RAJI <ror716@mail.usask.ca>
2019-11-14 08:32:42 -0600

    Header component

---
+++ src/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Navigation(){
+    return(
+        <div>
+            <div>
+            <p> KARO </p>
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
