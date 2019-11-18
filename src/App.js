df96085722827ce4d696de6218c63e9b9dcf2f3d
RIDWAN RAJI <ror716@mail.usask.ca>
2019-11-18 15:19:30 -0600

    Inline styles with style properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import CheckboxComponent from './Components/CheckboxComp';
+import CheckboxComponent from './Components/CheckboxComp'
 import NavComponent from './Components/NavComponent'
+import MyInfo from './Components/MyInfo'
 
 function App() {
   return (
     <div>
       <NavComponent />
+      <MyInfo />
       <CheckboxComponent />
       <CheckboxComponent />
       <CheckboxComponent />
--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Myinfo() {
+  const styles = {
+    color: "#8b6e53",
+    textAlign: "center"
+  }
   return (
-    <div>
+    <div style={styles}>
         <h1> My name is Ridwan Raji</h1>
         <p> I just finished my BSc Computer Science with a minor in Sociology. i am currently learning
             I'm currently learning React.JS which is what i am using to write this right now.
