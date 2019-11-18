e1bdba147b1dcfd3a15cf2e398f48e44e943977d
RIDWAN RAJI <ror716@mail.usask.ca>
2019-11-18 14:17:53 -0600

    change headerComponent name to NavCompnent

---
+++ src/Components/NavComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Styles/NavComponent.css";
+
+function NavComponent() {
+    return (
+        <header className="nav-component"> NO BUENO </header>
+    )
+}
+
+export default NavComponent;
