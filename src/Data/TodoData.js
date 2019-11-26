c6ea832895969f298fb23f9ac7bc96d832d94aaa
RIDWAN RAJI <ror716@mail.usask.ca>
2019-11-26 07:18:09 -0600

    todo app data

---
+++ src/Data/TodoData.js
@@ -0,0 +1,34 @@
+const TodoData = [
+    {
+        id: 1,
+        text:"Update Resume",
+        completed: false
+    },
+    {
+        id: 2,
+        text:"Graduate from University",
+        completed: true
+    },
+    {
+        id: 3,
+        text:"Apply for jobs",
+        completed: false
+    },
+    {
+        id: 4,
+        text:"Go on a trip alone",
+        completed: false
+    },
+    {
+        id: 5,
+        text:"Start learning a new language",
+        completed: true
+    },
+    {
+        id: 6,
+        text:"Start a react project",
+        completed: true
+    }
+]
+
+export default TodoData
