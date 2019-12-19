6099347e8f16599596b500c2206a0ea8beb91790
RIDWAN RAJI <ror716@mail.usask.ca>
2019-12-18 19:21:38 -0600

    add event handler to the checkbox

--- src/Components/TodoComponent.js
+++ src/Components/TodoComponent.js
@@ -4,7 +4,7 @@ class TodoComponent extends React.Component {
     render(){
         return(
             <div>
-                <input type="checkbox" checked={this.props.items.completed} onChange ={() => console.log("Changed")} />
+                <input type="checkbox" checked={this.props.items.completed} onChange ={() => console.log("i was Changed")} />
                 <p> {this.props.items.text} </p>
             </div>
         )
